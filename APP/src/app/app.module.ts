e0ea7722cfd3df852e87c05bb87a90838f36a1e4
Hellen Cristina Gurgacz <gurgaczzhellen@gmail.com>
2021-11-25 20:48:00 -0300

    alterações

--- APP/src/app/app.module.ts
+++ APP/src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http"
 import { AppComponent } from './app.component';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { CadastrarFarmaciaComponent } from './components/views/cadastrar-farmacia/cadastrar-farmacia.component';
 import { CadastrarProdutoComponent } from './components/views/cadastrar-produto/cadastrar-produto.component';
 import { CadastrarUsuarioComponent } from './components/views/cadastrar-usuario/cadastrar-usuario.component';
 import { ListarProdutoComponent } from './components/views/listar-produto/listar-produto.component';
-
-
 
 
 @NgModule({
@@ -24,4 +23,5 @@ import { ListarProdutoComponent } from './components/views/listar-produto/listar
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
