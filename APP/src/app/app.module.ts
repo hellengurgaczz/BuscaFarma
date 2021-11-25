43db1be965a5c8eba0f20ad0fdf57207e422e6ba
Hellen Cristina Gurgacz <gurgaczzhellen@gmail.com>
2021-11-25 20:24:54 -0300

    adicionando importacoes e declarations no module

--- APP/src/app/app.module.ts
+++ APP/src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http"
+import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {HttpClientModule} from "@angular/common/http"
+import { CadastrarFarmaciaComponent } from './components/views/cadastrar-farmacia/cadastrar-farmacia.component';
+import { CadastrarProdutoComponent } from './components/views/cadastrar-produto/cadastrar-produto.component';
+import { CadastrarUsuarioComponent } from './components/views/cadastrar-usuario/cadastrar-usuario.component';
+import { ListarProdutoComponent } from './components/views/listar-produto/listar-produto.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,CadastrarFarmaciaComponent,CadastrarProdutoComponent,CadastrarUsuarioComponent,ListarProdutoComponent
   ],
   imports: [
     BrowserModule,
